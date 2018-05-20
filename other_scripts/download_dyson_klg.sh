d622aaf3b9b85f491b5f742f64aa38acc24b2fec
Jordan Stewart <jordy.stewart@hotmail.co.uk>
2018-05-20 14:03:33 +0000

    added scripts for installing stopwatch and downloading the log file

---
+++ other_scripts/download_dyson_klg.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cd ../../log_files
+
+wget http://www.doc.ic.ac.uk/~sleutene/datasets/elasticfusion/dyson_lab.klg
---
+++ other_scripts/install_stopwatch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd .. # get out of other_scripts
+cd .. #get out of elasticfusion
+
+#Need to install QT 4.x+ first
+sudo apt-get install -y qt4-dev-tools
+#Also need NCurses library
+sudo apt-get install -y libncurses-dev
+
+#Then we can clone n install etccc
+git clone https://github.com/mp3guy/Stopwatch
+cd Stopwatch
+
+#Make the build directory
+mkdir build
+cd build/
+
+cmake ../src/
+make -j4
+
+#After this point all should be good to run ./StopwatchViewer
