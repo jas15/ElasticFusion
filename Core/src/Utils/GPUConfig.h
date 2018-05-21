e9a536c478c8b7c57bea9c05db31286ed145d956
Jordan Stewart <jordy.stewart@hotmail.co.uk>
2018-05-21 14:17:13 +0000

    altered GPUTest cmakelist to run on tx2. note removed msse2 msse3 flags.

--- Core/src/Utils/GPUConfig.h
+++ Core/src/Utils/GPUConfig.h
@@ -84,7 +84,7 @@ class GPUConfig
             rgbStepMap["GeForce GTX 970"] = std::pair<int, int>(160, 64);
             rgbResMap["GeForce GTX 970"] = std::pair<int, int>(128, 272);
             so3StepMap["GeForce GTX 970"] = std::pair<int, int>(96, 64);
-               
+
             icpStepMap["GeForce GTX 965M"] = std::pair<int, int>(256, 32);
             rgbStepMap["GeForce GTX 965M"] = std::pair<int, int>(224, 16);
             rgbResMap["GeForce GTX 965M"] = std::pair<int, int>(384, 480);
@@ -119,6 +119,11 @@ class GPUConfig
             rgbStepMap["GeForce GTX 1070"] = std::pair<int, int>(128, 96);
             rgbResMap["GeForce GTX 1070"] = std::pair<int, int>(256, 464);
             so3StepMap["GeForce GTX 1070"] = std::pair<int, int>(256, 48);
+
+            //icpStepMap["NVIDIA Tegra X2"] = std::pair<int, int>(64, 240);
+            //rgbStepMap["NVIDIA Tegra X2"] = std::pair<int, int>(128, 96);
+            //rgbResMap ["NVIDIA Tegra X2"] = std::pair<int, int>(256, 464);
+            //so3StepMap["NVIDIA Tegra X2"] = std::pair<int, int>(256, 48);
 
             if(icpStepMap.find(dev) == icpStepMap.end())
             {
