17e822a67f5dc7e704f9494fc8c95707ab4128bd
Jordan Stewart <jordy.stewart@hotmail.co.uk>
2018-05-23 17:12:33 +0000

    added new flags to compiler. modified plotter script to accept input from term call. need to change save file names of Stopwatch

--- other_scripts/plotter.py
+++ other_scripts/plotter.py
@@ -1,18 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-log = open('log_files/refresh_rate.txt')
+fileLocs = sys.argv[1:]
+
+log = open(fileLocs[0])
 dataRaw1 = log.readlines()
 log.close()
-log = open('log_files/refresh_rate.txt')
+log = open(fileLocs[1])
 dataRaw2 = log.readlines()
 log.close()
-log = open('log_files/refresh_rate.txt')
-dataRaw3 = log.readlines()
-log.close()
-log = open('log_files/refresh_rate.txt')
-dataRaw4 = log.readlines()
-log.close()
+#log = open('/home/nvidia/IndProj/EF/log_files/refresh_rate.txt')
+#dataRaw3 = log.readlines()
+#log.close()
+#log = open('/home/nvidia/IndProj/EF/log_files/refresh_rate.txt')
+#dataRaw4 = log.readlines()
+#log.close()
 
 length = len(dataRaw1)
 frames = list(range(length))
@@ -21,19 +24,19 @@ fig = plt.figure(1)
 
 dataHz1 = np.empty([length], dtype=np.float)
 dataHz2 = np.empty([length], dtype=np.float)
-dataHz3 = np.empty([length], dtype=np.float)
-dataHz4 = np.empty([length], dtype=np.float)
+#dataHz3 = np.empty([length], dtype=np.float)
+#dataHz4 = np.empty([length], dtype=np.float)
 
 for i in frames:
     dataHz1[i] = float(dataRaw1[i])
     dataHz2[i] = float(dataRaw2[i])
-    dataHz3[i] = float(dataRaw3[i])
-    dataHz4[i] = float(dataRaw4[i])
+#    dataHz3[i] = float(dataRaw3[i])
+#    dataHz4[i] = float(dataRaw4[i])
 
 plt.plot(frames, dataHz1, 'g', label='New implementation, max-P')
 plt.plot(frames, dataHz2, 'bo', label='New implementation, max-Q')
-plt.plot(frames, dataHz3, 'mx', label='Old implementation, max-P')
-plt.plot(frames, dataHz4, 'c-', label='Old implementation, max-Q')
+#plt.plot(frames, dataHz3, 'mx', label='Old implementation, max-P')
+#plt.plot(frames, dataHz4, 'c-', label='Old implementation, max-Q')
 
 plt.title("Performance of ElasticFusion on NVidia Jetson TX2\nRunning Dataset \"SOMETHING\"")
 plt.xlabel("Frame number")
